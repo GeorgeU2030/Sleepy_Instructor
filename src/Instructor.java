dbb9a06322f3592b471c14fa7f1178ce3b0ec12f
Diana Balanta <dianalorenabalanta@gmail.com>
2024-03-19 01:47:05 -0500

    feat: poll has been added

--- src/Instructor.java
+++ src/Instructor.java
@@ -1,24 +1,29 @@
 import java.util.concurrent.Semaphore;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class Instructor {
-
+class Instructor {
     private Semaphore mutex = new Semaphore(1);
     private Semaphore studentWaiting = new Semaphore(0);
     private Semaphore chairAvailable = new Semaphore(3);
+    private Semaphore monitorSleeping = new Semaphore(0); // Semáforo para despertar al monitor
     private int studentCount = 0;
-    private int lastStudentId = -1; 
+    private int lastStudentId = -1;
+    private Queue<Integer> studentQueue = new LinkedList<>(); // Cola para mantener el orden de llegada de los estudiantes
 
     public void studentArrived(int id) throws InterruptedException {
         mutex.acquire();
         if (studentCount == 0) {
             System.out.println("Arrive the student " + id);
             System.out.println("Instructor is helping the student " + id);
+
             studentCount++;
             mutex.release();
-        } else if (studentCount < 3) {
+        } else if (studentCount < 4) {
             System.out.println("Busy Instructor, arrive the student " + id);
             chairAvailable.acquire();
             System.out.println("Student " + id + " sit in the chair " + studentCount);
+            studentQueue.add(id); // Agregar estudiante a la cola
             studentCount++;
             studentWaiting.release();
             mutex.release();
@@ -32,13 +37,26 @@ public class Instructor {
         mutex.acquire();
         studentCount--;
         System.out.println("Instructor has helped the student " + id);
-        lastStudentId = id; 
+
         chairAvailable.release();
-        if (studentCount > 0) {
-            System.out.println("Instructor is helping the next student");
+        if (!studentQueue.isEmpty()) { // Si hay estudiantes esperando en la cola
+            int nextStudent = studentQueue.poll(); // Obtener el siguiente estudiante de la cola
+            System.out.println("Instructor is helping the next student in queue: " + nextStudent);
+            lastStudentId = nextStudent;
             studentWaiting.release();
+        } else {
+            System.out.println("No more students waiting, Instructor is sleeping");
+            monitorSleeping.release(); // Permitir que el monitor vuelva a dormir si no hay estudiantes esperando
         }
         mutex.release();
     }
 
+    public void monitorSleep() throws InterruptedException {
+        System.out.println("Instructor is sleeping");
+        monitorSleeping.acquire(); // Esperar hasta que se despierte el monitor
+    }
+    public void wakeUpMonitor() {
+        System.out.println("Instructor ha sido despertado");
+        monitorSleeping.release(); // Despertar al monitor
+    }
 }
